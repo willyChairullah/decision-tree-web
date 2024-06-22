871e32df185f9a28b78d4f0bf168e0cf17e45363
Wichtra <willy.chairullah@gmail.com>
2024-06-22 21:59:19 +0700

    remove temp file

--- main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-import streamlit as st
-
-st.page_link("main.py", label="Home", icon="🏠")
-st.page_link("pages/page3.py", label="Page 1", icon="1️⃣")
-st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
-st.page_link("http://www.google.com", label="Google", icon="🌎")
